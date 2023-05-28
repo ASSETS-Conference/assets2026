6c5017fccad55bb2835216b5f9b736fe0110f0a6
oliveralonzo <oliveralonzo@users.noreply.github.com>
2023-05-28 13:54:51 -0400

    Fix logo.html URL

--- assets/js/logo.js
+++ assets/js/logo.js
@@ -1,5 +1,5 @@
 $(function () {
-    $(".logo-wrapper").load("../../logo.html", complete = function () {
+    $(".logo-wrapper").load("logo.html", complete = function () {
         $(".logo").hide().fadeIn(1500);
     });
 });
