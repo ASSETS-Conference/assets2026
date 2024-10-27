69ed6a338127e3a8c5859d23dbf4125e5d10b79b
Calua de Lacerda Pataca <calua.pataca@gmail.com>
2024-10-27 13:15:08 -0400

    abs ref

--- setup.js
+++ setup.js
@@ -1,10 +1,10 @@
-fetch('footer.html')
+fetch('/footer.html')
   .then((response) => response.text())
   .then((html) => {
     document.getElementById('footer-container').innerHTML = html
   })
 
-fetch('menu.html')
+fetch('/menu.html')
   .then((response) => response.text())
   .then((html) => {
     const menuEl = document.getElementById('nav-container')
@@ -21,12 +21,11 @@ fetch('menu.html')
     document.body.appendChild(script)
   })
 
-
 // Add small caps to all-caps words
-document.querySelectorAll('div.text-block p, div.text-block li, .grid-item p').forEach(function(element) {
-  element.innerHTML = element.innerHTML.replace(/\b([A-Z]{2,})\b/g, function(match) {
-    return `<span class="small-caps">${match}</span>`;
-  });
-});
-
-  
+document
+  .querySelectorAll('div.text-block p, div.text-block li, .grid-item p')
+  .forEach(function (element) {
+    element.innerHTML = element.innerHTML.replace(/\b([A-Z]{2,})\b/g, function (match) {
+      return `<span class="small-caps">${match}</span>`
+    })
+  })
